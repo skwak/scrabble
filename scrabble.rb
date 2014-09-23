3f72ea97c504dcd7cd363af42806d881547920a7
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-09-23 12:34:04 -0700

    bronze level cleanup

--- scrabble.rb
+++ scrabble.rb
@@ -1,26 +1,3 @@
-# Minimum 8 specs
-#
-# Scrabble class
-#
-# self.score(word) returns the total score value for the given word (case insensitive)
-# self.highest_score_from(array_of_words) returns the word in the array with the
-# highest score.
-# Note that it’s better to use fewer tiles, so if the top score is tied between
-#  multiple words, pick the one with the fewest letters.
-# But there is a bonus for using all seven letters. If one of the highest scores
-#  uses all seven letters, pick that one
-# But if the there are multiple words that are the same score and same length,
-# pick the first one in supplied list
-
-# #A, E, I, O, U, L, N, R, S, T	1
-# D, G	2
-# B, C, M, P	3
-# F, H, V, W, Y	4
-# K	5
-# J, X	8
-# Q, Z	10
-
-
 class Scrabble
   attr_accessor :word
 
@@ -40,16 +17,7 @@ class Scrabble
         find_top_word
       end
     end
-
     find_top_word
-
-    # top_array = array_of_words.find_all { |word| score(word) == highest_score}
-    # top_array.sort.last
-    # if (top_array.length = 2)
-    #   top_array.find { |word| word if tied_score(word, highest_score_word)}
-    # else
-    #   top_array[0]
-    # end
   end
 
   def self.tied_score(word, another_word)
